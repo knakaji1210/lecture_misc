654158c53910a5e569102f662479a6b7a31a0656
knakaji <knakaji@mac.titech.ac.jp>
2025-02-18 22:55:17 +0900

    ndarray

--- ndarray/timeit_test.py
+++ ndarray/timeit_test.py
@@ -6,5 +6,7 @@ def test(n):
 n = 10000
 loop = 1000
 
-result = timeit.timeit('test(n)', globals=globals(), number=loop)
+# result = timeit.timeit('test(n)', globals=globals(), number=loop)
+result = timeit.timeit(lambda: test(n), number=loop)
+
 print(result / loop)
